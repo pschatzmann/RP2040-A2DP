29c1437747425e3062b967e9b901000ca9ffdf0e
pschatzmann <phil.schatzmann@gmail.com>
2023-03-16 10:17:54 +0100

    Extend codecs

--- src/A2DPCodecs.h
+++ src/A2DPCodecs.h
@@ -26,7 +26,7 @@ struct media_codec_configuration_sbc_t {
     LOGI("- block_length: %d", block_length);
     LOGI("- subbands: %d", subbands);
     LOGI("- allocation_method: %d", allocation_method);
-    LOGI("- bitpool_value [%d, %d]", min_bitpool_value,max_bitpool_value);
+    LOGI("- bitpool_value [%d, %d]", min_bitpool_value, max_bitpool_value);
   }
 };
 
@@ -36,13 +36,18 @@ struct media_codec_configuration_sbc_t {
  */
 class A2DPEncoder {
 public:
-  virtual void *config() = 0;
+  virtual uint8_t *config() = 0;
   virtual int configSize() = 0;
+  virtual void setValues(uint16_t cid, uint8_t *packet, uint16_t size) = 0;
   virtual void begin() = 0;
   virtual uint8_t *codecCapabilities() = 0;
   virtual int codecCapabilitiesSize() = 0;
   virtual AudioEncoder &encoder() = 0;
   virtual void dump() = 0;
+  virtual int frameLengthEncoded() = 0;
+  virtual int frameLengthDecoded() = 0;
+  virtual AudioBaseInfo audioInfo() = 0;
+  virtual avdtp_media_codec_type_t codecType() = 0;
 };
 
 /**
@@ -51,7 +56,7 @@ public:
  */
 class A2DPDecoder {
 public:
-  virtual void *config() = 0;
+  virtual uint8_t *config() = 0;
   virtual int configSize() = 0;
   virtual void begin() = 0;
   virtual uint8_t *codecCapabilities() = 0;
@@ -65,9 +70,77 @@ public:
  */
 class A2DPEncoderSBC : public A2DPEncoder {
 public:
-  void *config() override { return &sbc_config; }
+  A2DPEncoderSBC(){
+    TRACEI();
+  }
+  uint8_t *config() override { return media_sbc_codec_configuration; }
 
-  int configSize() override { return sizeof(sbc_config); }
+  int configSize() override { return sizeof(media_sbc_codec_configuration); }
+
+  void setValues(uint16_t cid, uint8_t *packet, uint16_t size) override {
+    avdtp_channel_mode_t channel_mode;
+    uint8_t allocation_method;
+
+    sbc_config.reconfigure =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_reconfigure(
+            packet);
+    sbc_config.num_channels =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_num_channels(
+            packet);
+    sbc_config.sampling_frequency =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_sampling_frequency(
+            packet);
+    sbc_config.block_length =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_block_length(
+            packet);
+    sbc_config.subbands =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_subbands(
+            packet);
+    sbc_config.min_bitpool_value =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_min_bitpool_value(
+            packet);
+    sbc_config.max_bitpool_value =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_max_bitpool_value(
+            packet);
+
+    channel_mode = (avdtp_channel_mode_t)
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_channel_mode(
+            packet);
+    allocation_method =
+        a2dp_subevent_signaling_media_codec_sbc_configuration_get_allocation_method(
+            packet);
+
+    LOGI("A2DP Source: Received SBC codec configuration, sampling "
+         "frequency %u, a2dp_cid 0x%02x, local seid 0x%02x, remote seid "
+         "0x%02x.",
+         sbc_config.sampling_frequency, cid,
+         a2dp_subevent_signaling_media_codec_sbc_configuration_get_local_seid(
+             packet),
+         a2dp_subevent_signaling_media_codec_sbc_configuration_get_remote_seid(
+             packet));
+
+    // Adapt Bluetooth spec definition to SBC Encoder expected input
+    sbc_config.allocation_method =
+        (btstack_sbc_allocation_method_t)(allocation_method - 1);
+    switch (channel_mode) {
+    case AVDTP_CHANNEL_MODE_JOINT_STEREO:
+      sbc_config.channel_mode = SBC_CHANNEL_MODE_JOINT_STEREO;
+      break;
+    case AVDTP_CHANNEL_MODE_STEREO:
+      sbc_config.channel_mode = SBC_CHANNEL_MODE_STEREO;
+      break;
+    case AVDTP_CHANNEL_MODE_DUAL_CHANNEL:
+      sbc_config.channel_mode = SBC_CHANNEL_MODE_DUAL_CHANNEL;
+      break;
+    case AVDTP_CHANNEL_MODE_MONO:
+      sbc_config.channel_mode = SBC_CHANNEL_MODE_MONO;
+      break;
+    default:
+      btstack_assert(false);
+      break;
+    }
+    dump();
+  }
 
   void begin() override {
     // set encoder parameters
@@ -83,8 +156,22 @@ public:
   }
   AudioEncoder &encoder() override { return sbc_codec; }
   void dump() override { sbc_config.dump(); };
+  int frameLengthEncoded() override { return sbc_codec.frameLength(); };
+  int frameLengthDecoded() override { return sbc_codec.codeSize(); };
+  virtual AudioBaseInfo audioInfo() {
+    AudioBaseInfo info;
+    info.bits_per_sample = 16;
+    info.channels = sbc_config.num_channels;
+    info.sample_rate = sbc_config.sampling_frequency;
+    return info;
+  }
+
+  avdtp_media_codec_type_t codecType() {
+    return AVDTP_CODEC_SBC;
+  }
 
 protected:
+  uint8_t media_sbc_codec_configuration[4];
   media_codec_configuration_sbc_t sbc_config;
   SBCEncoder sbc_codec;
 
@@ -95,14 +182,16 @@ protected:
       2, 53};
 };
 
+
+
 /**
  * @brief SBC Decoder implementation
  * @author Phil Schatzmann
  */
 class A2DPDecoderSBC : public A2DPDecoder {
 public:
-  void *config() override { return &sbc_config; }
-  int configSize() override { return sizeof(sbc_config); }
+  uint8_t *config() override { return media_sbc_codec_configuration; }
+  int configSize() override { return sizeof(media_sbc_codec_configuration); }
   void begin() override {}
   uint8_t *codecCapabilities() override { return media_sbc_codec_capabilities; }
   int codecCapabilitiesSize() override {
@@ -111,6 +200,7 @@ public:
   AudioDecoder &decoder() override { return sbc_codec; }
 
 protected:
+  uint8_t media_sbc_codec_configuration[4];
   media_codec_configuration_sbc_t sbc_config;
   SBCDecoder sbc_codec;
   uint8_t media_sbc_codec_capabilities[4] = {
